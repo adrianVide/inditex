8364c008d2a9617f31db8a32420883c28341564c
adrian <adrian.vide.paris@gmail.com>
2024-04-27 17:33:35 +0200

    Add react-router-dom dependency and update App.js for routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route, Redirect, Navigate
+} from "react-router-dom";
+import Home from './Pages/Home';
+import Podcast from './Pages/Podcast';
+import Episode from './Pages/Episode';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/podcast/:podcastId" element={<Podcast />} />
+        <Route path="/podcast/:podcastId/episode/:episodeId" element={<Episode />} />
+      </Routes>
+    </Router>
   );
+
 }
 
 export default App;
